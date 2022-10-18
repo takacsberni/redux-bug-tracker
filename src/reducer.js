4275abf26c9038582b7e2fedb9a12fb8c24cda38
takacsberni <takacsberni@gmail.com>
2022-10-18 17:36:39 +0200

    fix: use variables from actionTypes.js instead of hardcoded action types

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,11 @@
+// import {BUG_ADDED, BUG_REMOVED} from "./actionTypes";
+import * as actions from './actionTypes';
+
 let lastId = 0;
 
 export default function reducer(state = [], action){
     switch (action.type){
-        case "bugAdded":
+        case actions.BUG_ADDED:
             return [
                 ...state,
                 {
@@ -11,7 +14,7 @@ export default function reducer(state = [], action){
                     resolved: false
                 }
             ];
-        case "bugRemoved":
+        case actions.BUG_REMOVED:
             return state.filter(bug => bug.id !== action.payload.id);
         default:
             return state;
