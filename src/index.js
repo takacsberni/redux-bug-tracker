49ec263c1300da4dcfe549dcd76e7299c83771f6
takacsberni <takacsberni@gmail.com>
2022-10-18 19:10:12 +0200

    fix: use parameters in actions

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import store from './store'
-import * as actions from './actionTypes';
+
+import {bugAdded, bugRemoved} from "./actions";
 
 const unsubscribe = store.subscribe( () => {
     console.log("Store changed!", store.getState());
 }) //subscribe() always returns a function. Invoke the function returned by it lead to unsubscribe the change listener
 
 
-store.dispatch({
-    type: actions.BUG_ADDED,
-    payload: {
-        description: "Bug1"
-    }
-})
-
+store.dispatch(bugAdded("Bug 2"));
 unsubscribe();
-store.dispatch({
-    type: actions.BUG_REMOVED,
-    payload: {
-        id: 1
-    }
-})
+store.dispatch(bugRemoved(1));
 
 console.log(store.getState());
