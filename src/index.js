3cc22b6ff9bf1b8e8c3c95e107929aa7cb542f3a
takacsberni <takacsberni@gmail.com>
2022-10-24 11:31:07 +0200

    feat: implement the bug resolving option

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import store from './store'
 
-import {bugAdded, bugRemoved} from "./actions";
+import {bugAdded, bugRemoved, bugResolved} from "./actions";
 
 const unsubscribe = store.subscribe( () => {
     console.log("Store changed!", store.getState());
 }) //subscribe() always returns a function. Invoke the function returned by it lead to unsubscribe the change listener
 
 
-store.dispatch(bugAdded("Bug 2"));
-unsubscribe();
+store.dispatch(bugAdded("Bug 1"));
+// unsubscribe();
 store.dispatch(bugRemoved(1));
+store.dispatch(bugAdded("Bug 2"));
+store.dispatch(bugResolved(2));
 
 console.log(store.getState());
